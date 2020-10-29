b083419d15b6bd3319472b800ea04279685a74fc
ip99202 <ip99202@naver.com>
2020-10-29 21:25:03 +0900

    Create post

---
+++ src/main/java/com/beginfill/board/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.beginfill.board.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
