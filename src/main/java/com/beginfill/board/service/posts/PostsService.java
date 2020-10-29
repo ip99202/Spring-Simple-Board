061df9cf0c15ecbe4c703f47c1d632b50b2bcb54
ip99202 <ip99202@naver.com>
2020-10-29 21:29:27 +0900

    Create PostService

---
+++ src/main/java/com/beginfill/board/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.beginfill.board.service.posts;
+
+
+import com.beginfill.board.domain.posts.PostsRepository;
+import com.beginfill.board.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
